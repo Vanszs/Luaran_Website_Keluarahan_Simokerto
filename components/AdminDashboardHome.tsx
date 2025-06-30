8013123b112d18f4b8af4157a747fdac5d2d5d67
Vanszs <22081010153@student.upnjatim.ac.id>
2025-06-30 20:48:47 +0700

    fix : fix typo

--- components/AdminDashboardHome.tsx
+++ components/AdminDashboardHome.tsx
@@ -247,7 +247,7 @@ export default function DashboardHome({ onViewChange }: DashboardHomeProps) {
                   
                   <Stack direction="row" spacing={2} alignItems="center">
                     <Chip
-                      label="Warga Simokerto"
+                      label="Admin Kelurahan"
                       color="primary"
                       sx={{ 
                         fontWeight: 600,
